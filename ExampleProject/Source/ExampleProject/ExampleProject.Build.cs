a95710155ddfeb0abf434f50b24d8e6781515f9e
y-akahori-ramen <y.akahori.ramen@gmail.com>
2022-02-13 13:57:17 +0900

    不要なコード整理

--- ExampleProject/Source/ExampleProject/ExampleProject.Build.cs
+++ ExampleProject/Source/ExampleProject/ExampleProject.Build.cs
@@ -14,13 +14,5 @@ public class ExampleProject : ModuleRules
 
 		// Use gRPC module
 		PublicDependencyModuleNames.Add("gRPCForUE4");
-
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
-
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
 	}
 }
